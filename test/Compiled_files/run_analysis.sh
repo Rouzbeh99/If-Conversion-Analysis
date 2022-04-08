5f0503cb1fca4ed8d33002f5cdae13274c8140a4
Rouzbeh <47076354+Rouzbeh99@users.noreply.github.com>
2022-04-08 14:08:31 -0600

    Update run_analysis.sh

--- test/Compiled_files/run_analysis.sh
+++ test/Compiled_files/run_analysis.sh
@@ -3,7 +3,7 @@
 FILENAME="dijkstra"
 tmp="_norm"
 output=$FILENAME$tmp
-clang -g -O3 -fno-vectorize -fno-slp-vectorize -fno-unroll-loops -mllvm -phi-node-folding-threshold=0 -mllvm -two-entry-phi-node-folding-threshold=0 -mllvm -simplifycfg-hoist-common=false -mllvm -simplifycfg-sink-common=0 -mllvm -simplifycfg-hoist-cond-stores=false -mllvm -simplifycfg-merge-cond-stores=false -mllvm -simplifycfg-branch-fold-threshold=0 -mllvm -simplifycfg-branch-fold-common-dest-vector-multiplier=0  -S -emit-llvm $FILENAME.c -o $FILENAME.ll
+clang -g -O3 -fno-vectorize -fno-slp-vectorize -fno-unroll-loops -S -emit-llvm $FILENAME.c -o $FILENAME.ll
 opt $FILENAME.ll -S -o $output.ll
 
 
